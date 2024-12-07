c90ec1392fa4ef5011f71466341d96ece4624dbb
Ilia Petrov <ilia.petrov@netcracker.com>
2024-12-08 01:00:44 +0300

    fix issues

--- deployment/backend/main.py
+++ deployment/backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from common.config import logger
-from routers import qdrant, image
+from routers import qdrant
 
 app = FastAPI(title="api",  version="1.0.0")
 
